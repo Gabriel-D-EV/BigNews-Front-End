b0c197cbd7f4a06621a9c6b209b105e18036dbfd
Gabriel Silva <gabriel.silva_dev@outlook.com>
2024-01-09 21:41:57 -0300

    criação do Mock

---
+++ src/datas.js
@@ -0,0 +1,42 @@
+export const news = [
+    {
+        title: 'New News',
+        text: "lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor ",
+        image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpicsum.photos%2F&psig=AOvVaw0DP2-ssm5pz-S02q68NuNC&ust=1704933225579000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLCV1LbK0YMDFQAAAAAdAAAAABAD",
+        likes: 2,
+        comments: 5
+
+    },
+    {
+        title: 'New News 2',
+        text: "lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor ",
+        image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpicsum.photos%2F&psig=AOvVaw0DP2-ssm5pz-S02q68NuNC&ust=1704933225579000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLCV1LbK0YMDFQAAAAAdAAAAABAD",
+        likes: 2,
+        comments: 5
+
+    },
+    {
+        title: 'New News 3',
+        text: "lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor ",
+        image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpicsum.photos%2F&psig=AOvVaw0DP2-ssm5pz-S02q68NuNC&ust=1704933225579000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLCV1LbK0YMDFQAAAAAdAAAAABAD",
+        likes: 2,
+        comments: 5
+
+    },
+    {
+        title: 'New News 4',
+        text: "lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor ",
+        image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpicsum.photos%2F&psig=AOvVaw0DP2-ssm5pz-S02q68NuNC&ust=1704933225579000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLCV1LbK0YMDFQAAAAAdAAAAABAD",
+        likes: 2,
+        comments: 5
+
+    },
+    {
+        title: 'New News 5',
+        text: "lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor ",
+        image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpicsum.photos%2F&psig=AOvVaw0DP2-ssm5pz-S02q68NuNC&ust=1704933225579000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLCV1LbK0YMDFQAAAAAdAAAAABAD",
+        likes: 2,
+        comments: 5
+
+    }
+]
