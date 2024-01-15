6b84dc8f52e87550aeaa6056214f6a0f49c7f547
Gabriel Silva <gabriel.silva_dev@outlook.com>
2024-01-15 18:02:43 -0300

    Update datas.js

--- src/datas.js
+++ src/datas.js
@@ -1,50 +1,44 @@
 export const news = [
-    {
-        title: 'New News',
-        text: "lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor ",
-        image: "https://avatars.githubusercontent.com/u/140929886?v=4",
-        likes: 2,
-        comments: 5
-
-    },
-    {
-        title: 'New News 2',
-        text: "lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor ",
-        image: "https://avatars.githubusercontent.com/u/140929886?v=4",
-        likes: 2,
-        comments: 5
-
-    },
-    {
-        title: 'New News 3',
-        text: "lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor ",
-        image: "https://avatars.githubusercontent.com/u/140929886?v=4",
-        likes: 2,
-        comments: 5
-
-    },
-    {
-        title: 'New News 4',
-        text: "lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor ",
-        image: "https://avatars.githubusercontent.com/u/140929886?v=4",
-        likes: 2,
-        comments: 5
-
-    },
-    {
-        title: 'New News 5',
-        text: "lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor ",
-        image: "https://avatars.githubusercontent.com/u/140929886?v=4",
-        likes: 2,
-        comments: 5
-
-    },
-    {
-        title: 'New News 6',
-        text: "lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor ",
-        image: "https://avatars.githubusercontent.com/u/140929886?v=4",
-        likes: 2,
-        comments: 5
-
-    }
-]
+  {
+    title: "New News",
+    text: "lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adit lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor ",
+    image: "https://avatars.githubusercontent.com/u/140929886?v=4",
+    likes: 2,
+    comments: 5,
+  },
+  {
+    title: "New News 2",
+    text: "lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor ",
+    image: "https://avatars.githubusercontent.com/u/140929886?v=4",
+    likes: 2,
+    comments: 5,
+  },
+  {
+    title: "New News 3",
+    text: "lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor ",
+    image: "https://avatars.githubusercontent.com/u/140929886?v=4",
+    likes: 2,
+    comments: 5,
+  },
+  {
+    title: "New News 4",
+    text: "lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor ",
+    image: "https://avatars.githubusercontent.com/u/140929886?v=4",
+    likes: 2,
+    comments: 5,
+  },
+  {
+    title: "New News 5",
+    text: "lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor ",
+    image: "https://avatars.githubusercontent.com/u/140929886?v=4",
+    likes: 2,
+    comments: 5,
+  },
+  {
+    title: "New News 6",
+    text: "lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor ",
+    image: "https://avatars.githubusercontent.com/u/140929886?v=4",
+    likes: 2,
+    comments: 5,
+  },
+];
