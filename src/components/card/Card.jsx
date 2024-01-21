dc96b0d370bf440cc1cc4e992e2cc5cb24d939f5
Gabriel Silva <gabriel.silva_dev@outlook.com>
2024-01-21 00:23:15 -0300

    att

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,13 +1,20 @@
 export function Card({ news }) {
   return (
     <section>
-      <h2>{news.title}</h2>
-      <p>{news.text}</p>
-      <img src={news.image} alt="Imagem do Texto" />
-      <i className="bi bi-hand-thumbs-up"></i>
-      <span>{news.likes}</span>
-      <i className="bi bi-chat-left"></i>
-      <span>{news.comments}</span>
+      <article>
+        <div>
+          <h2>{news.title}</h2>
+          <p>{news.text}</p>
+        </div>
+        <img src={news.image} alt="Imagem do Texto" />
+      </article>
+
+      <article>
+        <i className="bi bi-hand-thumbs-up"></i>
+        <span>{news.likes}</span>
+        <i className="bi bi-chat-left"></i>
+        <span>{news.comments}</span>
+      </article>
     </section>
   );
 }
