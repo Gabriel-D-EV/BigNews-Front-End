29ba50e475140f26fe50fa102eab8a6005a6358f
Gabriel Silva <gabriel.silva_dev@outlook.com>
2024-01-23 03:39:57 -0300

    att

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -12,12 +12,13 @@ export function Navbar() {
             <li><a href="https://goanimes.net/" target="_blank">ANIMES</a></li>
             <li><a href="https://lermanga.org/" target="_blank">MANGÁS</a></li>
             <li><a href="https://huntersscan.xyz/" target="_blank">MANHWA</a></li>
+            <li><a href="#">Chat</a></li>
           </NavList>
         </div>
 
         <InputSpace>
           <i className="bi bi-search"></i>
-          <input type="text" placeholder="Pesquise"/>
+          <input type="text" placeholder="Pesquisar"/>
         </InputSpace>
 
         <Button><a href="#">ENTRAR</a></Button>
