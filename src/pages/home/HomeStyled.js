50a79b972855e45822b5e3b5d6ee658bcd74d1d7
Gabriel Silva <gabriel.silva_dev@outlook.com>
2024-03-08 21:36:14 -0300

    att

--- src/pages/home/HomeStyled.js
+++ src/pages/home/HomeStyled.js
@@ -8,23 +8,22 @@ export const HomeBody = styled.section`
   margin: 0 auto;
   border-radius: 0 0 15px 15px;
 
-  width: 70%;
+  width: 60%;
   
   background-color: #0f0f0fe9;
-  padding: 50px;
+  padding: 20px 150px;
   padding-top: 100px;
   box-shadow: 5px 20px 15px rgba(0, 0, 0, 0.70), 5px 12px 10px rgba(0, 0, 0, 0.70);
   margin-bottom: 80px;
   padding-top: 6.4%;
-  padding-bottom: 6%;
 `;
 
 export const HomeHeader = styled.section`
-  width: 70%;
+  width: 60%;
   display: flex;
   margin: auto;
   background-color: #0f0f0ffc;
   box-shadow: 5px 20px 25px rgba(0, 0, 0, 0.70), 5px 20px 25px rgba(0, 0, 0, 0.70);
-  padding: 50px;
-  padding-top: 10%;
+  padding: 20px 150px;
+  padding-top: 7%;
 `;
