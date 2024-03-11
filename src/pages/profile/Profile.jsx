89047ef33a436d3f0088ef3d6e7e48439b4a9ebb
Gabriel Silva <gabriel.silva_dev@outlook.com>
2024-03-11 03:04:11 -0300

    att profile

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,13 +1,40 @@
 import { useContext } from "react";
-import { UserContext } from "../../context/userContext";
-import { UserName } from "./profileStyled";
+import { UserContext } from "../../context/userContext.jsx";
+import {
+  AddNews,
+  BackgroundP,
+  IconP,
+  ImgP,
+  ProfileContainer,
+  ProfileHeader,
+  UserP,
+} from "./profileStyled.js";
 import { FooterFinal } from "../../components/footer/Footer.jsx";
+import bg from "../../images/background.jpg";
 
 export function Profile() {
   const { user } = useContext(UserContext);
   return (
     <>
-      <UserName>{user.name}</UserName>
+      <ProfileContainer>
+        <ProfileHeader>
+          <IconP>
+            <i class="bi bi-pencil-square"></i>
+          </IconP>
+
+          <BackgroundP src={bg} alt="background" />
+
+          <UserP>
+            <ImgP src={user.avatar} alt="foto de usuario" />
+            <h2>{user.name}</h2>
+            <h3>@{user.username}</h3>
+          </UserP>
+
+          <AddNews>
+            <i class="bi bi-plus-circle"></i>
+          </AddNews>
+        </ProfileHeader>
+      </ProfileContainer>
 
       <FooterFinal />
     </>
--- src/pages/profile/profileStyled.js
+++ src/pages/profile/profileStyled.js
@@ -1,10 +1,89 @@
 import styled from "styled-components";
 
-export const UserName = styled.h1`
-    font-size: 2rem;
-    padding-top: 5%;
-    font-weight: 600;
-    color: aquamarine;
-    text-align: center;
+export const ProfileContainer = styled.h1`
+    width: 60%;
+    padding: 20px 150px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 100px;
+    box-shadow: 5px 20px 15px rgba(0, 0, 0, 0.70), 5px 12px 10px rgba(0, 0, 0, 0.70);
+    background-color: #0f0f0fe9;
     height: 100vh;
+    border-radius: 10px;
+    margin: 20px auto;
+`;
+
+export const ProfileHeader = styled.header`
+    width: 100%;
+    margin-top: 2%;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    position: relative;
+    border-radius: 10px;
+    z-index: 0;
+    background-color: #0f0f0f;
+    box-shadow: 5px 20px 15px rgba(0, 0, 0, 0.70), 5px 12px 10px rgba(0, 0, 0, 0.70);
+`;
+
+export const IconP = styled.i`
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    color: aquamarine;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+
+    :hover {
+        color: blueviolet;
+    }
+`;
+
+export const ImgP = styled.img`
+    width: 15vw;
+    border-radius: 50%;
+    border: 10px solid #0f0f0f;
+    object-fit: cover;
+    object-position: center;
+`;
+
+export const BackgroundP = styled.img`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    object-fit: cover;
+    width: 100%;
+    height: 55%;
+    z-index: -1;
+    border-radius: 10px 10px 0 0;
+`;
+
+export const UserP = styled.div`
+    padding: 2rem;
+
+    h2 {
+        font-size: 3rem;
+        border-bottom: 1px solid;
+    }
+    h3 {
+        text-align: center;
+        font-size: 1.5rem;
+        color: blueviolet;
+    }
+`;
+
+export const AddNews = styled.div`
+    position: absolute;
+    bottom: 1rem;
+    right: 1rem;
+    color: aquamarine;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+
+    :hover {
+        color: blueviolet;
+    }
 `;
