a88faa0dc3ab59294ee16ecce78d07505d42dcdd
Gabriel Silva <gabriel.silva_dev@outlook.com>
2024-03-15 18:40:13 -0300

    att

--- src/pages/profile/profileStyled.js
+++ src/pages/profile/profileStyled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const ProfileContainer = styled.h1`
    
-    padding: 80px;
+    padding: 150px;
     display: flex;
     flex-direction: column;
     align-items: center;
