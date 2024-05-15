2886a369b0b723e0ab87f24de66c49e26b2b3939
Gabriel Silva <140929886+Gabriel-D-EV@users.noreply.github.com>
2024-05-14 22:53:10 -0300

    att

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -72,7 +72,7 @@ export function Navbar() {
                 </a>
               </li>
               <li>
-                <a href="https://leitordemanga.com/" target="_blank">
+                <a href="https://manhastro.com/" target="_blank">
                   MANGÁS
                 </a>
               </li>
